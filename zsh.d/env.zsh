a655c1e8b6122f5798bee208b3ac3ef79e91ea19
Haikel Guemar <hguemar@fedoraproject.org>
2015-09-05 22:51:51 +0200

    git: add shutup alias

--- zsh.d/env.zsh
+++ zsh.d/env.zsh
@@ -3,5 +3,5 @@ export PYTHONSTARTUP=~/.pythonrc.py
 export EDITOR='emacs'
 export VISUAL='emacs'
 export ALTERNATE_EDITOR=$(select_binary zile mg vim vi)
-export LD_LIBRARY_PATH=/usr/lib64/mpich2/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/lib64/mpich2/lib:/usr/local/lib:$LD_LIBRARY_PATH
 [ ! $VIRTUALENVWRAPPER_HOOK_DIR ] && which virtualenvwrapper.sh &>/dev/null && source virtualenvwrapper.sh
